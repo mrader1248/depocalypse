2ebe0b9242a6cb327e781d3e05ec18699294a7e0
Michael Rader <mrader1248@gmail.com>
2022-01-03 14:57:33 +0100

    added string representation of CommodityTransaction

--- backend/depot/models/commodity_transaction.py
+++ backend/depot/models/commodity_transaction.py
@@ -8,3 +8,10 @@ class CommodityTransaction(models.Model):
     commodity = models.ForeignKey(Commodity, on_delete=models.PROTECT)
     amount = models.IntegerField()
     best_before_date = models.DateField()
+
+    def __str__(self):
+        return (
+            f'{self.amount:+d} "{self.commodity.name}" (#{self.commodity.id})'
+            f', {self.transaction_timestamp}'
+            f', best before {self.best_before_date}'
+        )
