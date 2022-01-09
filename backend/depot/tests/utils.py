9f00620b20a223b8f409aacedaa5041d41c4b410
Michael Rader <mrader1248@gmail.com>
2022-01-09 19:24:09 +0100

    added docstring for create_commodity_with_transactions

--- backend/depot/tests/utils.py
+++ backend/depot/tests/utils.py
@@ -1,15 +1,35 @@
+import datetime
 from ..models.commodity import Commodity
 
 
-def create_commodity_with_transactions(name, calorific_value_in_kcal, transactions):
+def create_commodity_with_transactions(
+        name: str,
+        calorific_value_in_kcal: int,
+        transaction_data: list[tuple[datetime.datetime, int, datetime.date]]
+) -> Commodity:
+    """This function generates a `Commodity` object together with some
+    associated `CommodityTransaction` objects.
+
+    Args:
+        name (str): The name of the `Commodity` object to be created.
+        calorific_value_in_kcal (int): The calorific value of the
+            `Commodity` object to be created.
+        transaction_data: list[tuple[datetime.datetime, int, datetime.date]]:
+            A list containing timestamps, amounts and best before dates of the
+            transactions to be created.
+
+    Returns:
+        Commodity: The newly created `Commodity` object.
+    """
+
     commodity = Commodity.objects.create(
         name=name,
         calorific_value_in_kcal=calorific_value_in_kcal
     )
 
-    for transaction_timestamp, amount, best_before_date in transactions:
+    for timestamp, amount, best_before_date in transaction_data:
         commodity.commoditytransaction_set.create(
-            transaction_timestamp=transaction_timestamp,
+            transaction_timestamp=timestamp,
             amount=amount,
             best_before_date=best_before_date
         )
